e8ea5cbfd85a316a1a0b7a8800e63024d88b3345
Amir Fahimi <amir.fahimi.aviation@gmail.com>
2023-06-26 23:19:50 +0330

    changed mui style in crud form

--- src/component/CrudUser.jsx
+++ src/component/CrudUser.jsx
@@ -105,190 +105,200 @@ const CrudUser = () => {
             {isUpdating && <p>Loading..</p>}
 
             {isSuccessCreating && <p>User added successfull</p>}
-
             <Box
                 component="form"
                 sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                personal Information:
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="First Name"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    value={firstName}
-                />
+                    '& .MuiTextField-root': { m: 1, width: '65ch' },
+                    display: 'flex',
+                    flexFlow: 'row'
 
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="Last Name"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setLastName(e.target.value)}
-                    value={lastName}
-                />
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="Phone"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setPhone(e.target.value)}
-                    value={phone}
-                />
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="Email"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                />
-                <Stack spacing={1.5} sx={{ maxWidth: 200 }}>
-                    <FormLabel
-                        sx={(theme) => ({
-                            '--FormLabel-color': 'black',
-                        })}
-                    >
-                        Birth Date
-                        <Input
-                            type="date"
-                            slotProps={{
-                                input: {
-                                    min: '1985-06-07T00:00',
-                                    max: '2018-06-14T00:00',
-                                },
-                            }}
-                            onChange={(e) => setBirthday(e.target.value)}
-                            value={birthDay}
-                        />
-                    </FormLabel>
-
-                </Stack>
-                <TextField
-                    id="outlined-select-currency"
-                    select
-                    label="Gender"
-                    defaultValue="male"
-                    helperText="Please select your gender"
-
+                }}>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '35ch' },
+                        display: 'flex',
+                        flexFlow: 'column'
+                    }}
+                    noValidate
+                    autoComplete="off"
                 >
-                    {genderList.map((option) => (
-                        <MenuItem key={option.value} value={gender ? gender : option.value} onClick={(e) => setGender(e.currentTarget.innerText)}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="Job"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setJob(e.target.value)}
-                    value={job}
-                />
+                    personal Information:
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="First Name"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        value={firstName}
+                    />
 
-            </Box>
-            <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                Address:
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="Country"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setCountry(e.target.value)}
-                    value={country}
-                />
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="City"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setCity(e.target.value)}
-                    value={city}
-                />
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="Street"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setStreet(e.target.value)}
-                    value={street}
-                />
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="State"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setState(e.target.value)}
-                    value={state}
-                />
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="Zip Code"
-                    multiline
-                    maxRows={4}
-                    onChange={(e) => setZipCode(e.target.value)}
-                    value={zipCode}
-                />
-                <Stack spacing={1.5} sx={{ maxWidth: 200 }}>
-                    <FormLabel
-                        sx={(theme) => ({
-                            '--FormLabel-color': 'black',
-                        })}
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Last Name"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setLastName(e.target.value)}
+                        value={lastName}
+                    />
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Phone"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setPhone(e.target.value)}
+                        value={phone}
+                    />
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Email"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                    <Stack spacing={1.5} sx={{ maxWidth: 200 }}>
+                        <FormLabel
+                            sx={(theme) => ({
+                                '--FormLabel-color': 'black',
+                            })}
+                        >
+                            Birth Date
+                            <Input
+                                type="date"
+                                slotProps={{
+                                    input: {
+                                        min: '1985-06-07T00:00',
+                                        max: '2018-06-14T00:00',
+                                    },
+                                }}
+                                onChange={(e) => setBirthday(e.target.value)}
+                                value={birthDay}
+                            />
+                        </FormLabel>
+
+                    </Stack>
+                    <TextField
+                        id="outlined-select-currency"
+                        select
+                        label="Gender"
+                        defaultValue="male"
+                        helperText="Please select your gender"
+
                     >
-                        Longitude
-                        <Input
-                            type="number"
-                            defaultValue={2.5}
-                            slotProps={{
-                                input: {
+                        {genderList.map((option) => (
+                            <MenuItem key={option.value} value={gender ? gender : option.value} onClick={(e) => setGender(e.currentTarget.innerText)}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Job"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setJob(e.target.value)}
+                        value={job}
+                    />
 
-                                    min: 1,
-                                    max: 5,
-                                    step: 0.1,
-                                },
-                            }}
-                            onChange={(e) => setLongitude(e.target.value)}
-                            value={+longitude}
-                        />
-                    </FormLabel>
-                    <FormLabel
-                        sx={(theme) => ({
-                            '--FormLabel-color': 'black',
-                        })}
-                    >
-                        Latitude
-                        <Input
-                            type="number"
-                            defaultValue={2.5}
-                            slotProps={{
-                                input: {
+                </Box>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '35ch' },
+                        display: 'flex',
+                        flexFlow: 'column'
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    Address:
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Country"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setCountry(e.target.value)}
+                        value={country}
+                    />
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="City"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setCity(e.target.value)}
+                        value={city}
+                    />
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Street"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setStreet(e.target.value)}
+                        value={street}
+                    />
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="State"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setState(e.target.value)}
+                        value={state}
+                    />
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Zip Code"
+                        multiline
+                        maxRows={4}
+                        onChange={(e) => setZipCode(e.target.value)}
+                        value={zipCode}
+                    />
+                    <Stack spacing={1.5} sx={{ maxWidth: 200 }}>
+                        <FormLabel
+                            sx={() => ({
+                                '--FormLabel-color': 'black',
+                            })}
+                        >
+                            Longitude
+                            <Input
+                                type="number"
+                                defaultValue={2.5}
+                                slotProps={{
+                                    input: {
 
-                                    min: 1,
-                                    max: 5,
-                                    step: 0.1,
-                                },
-                            }}
-                            onChange={(e) => setLatitude(e.target.value)}
-                            value={+latitude}
-                        />
-                    </FormLabel>
-                </Stack>
+                                        min: 1,
+                                        max: 5,
+                                        step: 0.1,
+                                    },
+                                }}
+                                onChange={(e) => setLongitude(e.target.value)}
+                                value={+longitude}
+                            />
+                        </FormLabel>
+                        <FormLabel
+                            sx={() => ({
+                                '--FormLabel-color': 'black',
+                            })}
+                        >
+                            Latitude
+                            <Input
+                                type="number"
+                                defaultValue={2.5}
+                                slotProps={{
+                                    input: {
+
+                                        min: 1,
+                                        max: 5,
+                                        step: 0.1,
+                                    },
+                                }}
+                                onChange={(e) => setLatitude(e.target.value)}
+                                value={+latitude}
+                            />
+                        </FormLabel>
+                    </Stack>
+                </Box>
             </Box>
-
-
             <Box
                 component="form"
                 sx={{
@@ -307,7 +317,6 @@ const CrudUser = () => {
                     Submit
                 </Button>
             </Box>
-
         </>
     )
 }
